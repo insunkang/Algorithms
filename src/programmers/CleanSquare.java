0dc3bac1eb6a3047fffcd4f8dbd6cdbc178c8d8f
insunkang <33975738+insunkang@users.noreply.github.com>
2020-08-02 13:25:06 +0900

    solve CleanSquare

--- src/programmers/CleanSquare.java
+++ src/programmers/CleanSquare.java
@@ -3,8 +3,16 @@ package programmers;
 public class CleanSquare {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		int w =8;
+		int h =12;
+		int result=0;
+		
+		
+		for(int i=0;i<w;i++) {
+			result+=h*i/w;
+		}
+		System.out.println(result*2);
+		
 	}
 
 }
