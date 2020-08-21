966e3ec67fca1bbf84455fe2b0441a74293e0f3d
insunkang <33975738+insunkang@users.noreply.github.com>
2020-08-21 22:21:29 +0900

    update Surveillance

--- src/baekjoon/Surveillance.java
+++ src/baekjoon/Surveillance.java
@@ -13,7 +13,35 @@ public class Surveillance {
 		}
 		return result;
 	}
-
+	public static int zero(int[][] target, int m, int n, int left,int right, boolean[][] checked, int min) {
+		if (target[m][n]==1) {
+			
+			for (int i = 0; i < m; i++) {
+				for (int j = 0; j < n; j++) {
+					if (target[m][n]>0&&!checked[m][n]) {
+						
+					}
+				}
+			}
+			
+		}else if (target[m][n]==2) {
+			
+		}else if (target[m][n]==3) {
+			
+		}else if (target[m][n]==4) {
+			
+		}else if(target[m][n]==5){
+			 
+		}else {
+			int result = check(target,left,right);
+			if (min>result) {
+				min=result;
+			}
+			return result;
+		}
+	}
+	
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
