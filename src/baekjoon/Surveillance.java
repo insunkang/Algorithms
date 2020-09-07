66d1fe59f51102e3293b0f6dcb220ae76b5123ad
insunkang <33975738+insunkang@users.noreply.github.com>
2020-09-07 19:18:29 +0900

    perfectly solve surveillance

--- src/baekjoon/Surveillance.java
+++ src/baekjoon/Surveillance.java
@@ -1,6 +1,8 @@
 package baekjoon;
 
+import java.util.ArrayList;
 import java.util.LinkedList;
+import java.util.List;
 import java.util.Queue;
 import java.util.Scanner;
 
@@ -18,58 +20,67 @@ public class Surveillance {
 		return result;
 	}
 	
-	public static void surveil(int[][] target,int[][] origin, int m, int n, Queue<int[]> key, Queue<Integer> value,int i) {		
+	public static void surveil(int[][] target,int[][] origin, int m, int n, List<int[]> key, Queue<Integer> value,int i) {		
 		int[] left = {-1,0,1,0};
 		int[] right = {0,1,0,-1};
 		
-		while(i<key.size()) {		 	
-			int[] k = key.poll();
-			int v = value.poll();
+		if (key.size()==i) {
+			if (check(target, m, n)<result) {
+				result = check(target, m, n);
+			}
+			return;
+		}
+		
+		
+			int[] k = key.get(i);
+			int v = origin[k[0]][k[1]];
+			
 			if (v==1) {
+				
 				for (int j = 0; j < 4; j++) {
+					int[][] nextTarget = new int[m][n];
+					for (int l = 0; l < m; l++) {
+						for (int l2 = 0; l2 < n; l2++) {
+							nextTarget[l][l2]=target[l][l2];
+						}
+					}
 					int a =k[0]+left[j];
 					int b =k[1]+right[j];
-					while(a>=0&&b>=0&&a<m&&b<n&&target[a][b]!=6) {
-						if (target[a][b]==0) {
-							target[a][b]=8;
-							
-						}else {
-							
-						}
+					while(a>=0&&b>=0&&a<m&&b<n&&nextTarget[a][b]!=6) {
+						
+						
+						nextTarget[a][b]=8;													
 						a+=left[j];
 						b+=right[j];
 						
 					}
-					for (int fd = 0; fd < m; fd++) {
-						for (int j2 = 0; j2 < n; j2++) {
-							if (j2==n-1) {
-								System.out.println(target[fd][j2]);
-							}else {
-								System.out.print(target[fd][j2]);
-							}
-						}
-					}
-					System.out.println("---------------------");
-					if (check(target, m, n)<result) {
-						result = check(target, m, n);
-					}
-					surveil(target,origin,m,n,key,value,i);
 					
-					for (int l = 0; l < m; l++) {
-						for (int l2 = 0; l2 < n; l2++) {
-							target[l][l2]=origin[l][l2];
-						}
-					}
+					
+					surveil(nextTarget,origin,m,n,key,value,i+1);
+					
+//					for (int l = 0; l < m; l++) {
+//						for (int l2 = 0; l2 < n; l2++) {
+//							target[l][l2]=origin[l][l2];
+//							}
+//					}
 					
 				}
-				i++;
+				
 			}else if (v==2) {
+				
 				for (int j = 0; j < 2; j++) {
+					int[][] nextTarget = new int[m][n];
+					for (int l = 0; l < m; l++) {
+						for (int l2 = 0; l2 < n; l2++) {
+							nextTarget[l][l2]=target[l][l2];
+						}
+					}
 					int a = k[0]+left[j];
 					int b = k[1]+right[j];
 					
-					while(a>=0&&b>=0&&a<m&&b<n&&target[a][b]!=6) {
-						target[a][b]=8;
+					while(a>=0&&b>=0&&a<m&&b<n&&nextTarget[a][b]!=6) {
+						
+						nextTarget[a][b]=8;
 						a+=left[j];
 						b+=right[j];
 					}
@@ -77,41 +88,39 @@ public class Surveillance {
 					int c = k[0]+left[j+2];
 					int d = k[1]+right[j+2];
 					
-					while(c>=0&&d>=0&&c<m&&d<n&&target[c][d]!=6) {
-						target[c][d]=8;
+					while(c>=0&&d>=0&&c<m&&d<n&&nextTarget[c][d]!=6) {
+						
+						nextTarget[c][d]=8;
 						c+=left[j+2];
 						d+=right[j+2];
 					}
-					for (int fd = 0; fd < m; fd++) {
-						for (int j2 = 0; j2 < n; j2++) {
-							if (j2==n-1) {
-								System.out.println(target[fd][j2]);
-							}else {
-								System.out.print(target[fd][j2]);
-							}
-						}
-					}System.out.println("---------------------");
-					if (check(target, m, n)<result) {
-						result = check(target, m, n);
-					}
 					
-					surveil(target,origin,m,n,key,value,i);
-					for (int l = 0; l < m; l++) {
-						for (int l2 = 0; l2 < n; l2++) {
-							target[l][l2]=origin[l][l2];
-						}
-					}
+					
+					surveil(nextTarget,origin,m,n,key,value,i+1);
+//					for (int l = 0; l < m; l++) {
+//						for (int l2 = 0; l2 < n; l2++) {
+//							target[l][l2]=origin[l][l2];
+//						}
+//					}
 					
 					
 				}
-				i++;
+				
 			}else if (v==3) {
+				
 				for (int j = 0; j < 4; j++) {
+					int[][] nextTarget = new int[m][n];
+					for (int l = 0; l < m; l++) {
+						for (int l2 = 0; l2 < n; l2++) {
+							nextTarget[l][l2]=target[l][l2];
+						}
+					}
 					int a = k[0]+left[j];
 					int b = k[1]+right[j];
 					
-					while(a>=0&&b>=0&&a<m&&b<n&&target[a][b]!=6) {
-						target[a][b]=8;
+					while(a>=0&&b>=0&&a<m&&b<n&&nextTarget[a][b]!=6) {
+						
+						nextTarget[a][b]=8;
 						a+=left[j];
 						b+=right[j];
 					}
@@ -122,40 +131,39 @@ public class Surveillance {
 					
 					
 					
-					while(c>=0&&d>=0&&c<m&&d<n&&target[c][d]!=6) {
-						target[c][d]=8;
+					while(c>=0&&d>=0&&c<m&&d<n&&nextTarget[c][d]!=6) {
+						
+						nextTarget[c][d]=8;
 						c+=left[(j+1)%4];
 						d+=right[(j+1)%4];
 					}
-					for (int fd = 0; fd < m; fd++) {
-						for (int j2 = 0; j2 < n; j2++) {
-							if (j2==n-1) {
-								System.out.println(target[fd][j2]);
-							}else {
-								System.out.print(target[fd][j2]);
-							}
-						}
-					}System.out.println("---------------------");
-					if (check(target, m, n)<result) {
-						result = check(target, m, n);
-					}
 					
-					surveil(target,origin,m,n,key,value,i);
 					
+					
+					surveil(nextTarget,origin,m,n,key,value,i+1);
+					
+//					for (int l = 0; l < m; l++) {
+//						for (int l2 = 0; l2 < n; l2++) {
+//							target[l][l2]=origin[l][l2];
+//						}
+//					}
+				}
+				
+			}else if (v==4) {
+				
+				for (int j = 0; j < 4; j++) {
+					int[][] nextTarget = new int[m][n];
 					for (int l = 0; l < m; l++) {
 						for (int l2 = 0; l2 < n; l2++) {
-							target[l][l2]=origin[l][l2];
+							nextTarget[l][l2]=target[l][l2];
 						}
 					}
-				}
-				i++;
-			}else if (v==4) {
-				for (int j = 0; j < 4; j++) {
 					int a = k[0]+left[j];
 					int b = k[1]+right[j];
 					
-					while(a>=0&&b>=0&&a<m&&b<n&&target[a][b]!=6) {
-						target[a][b]=8;
+					while(a>=0&&b>=0&&a<m&&b<n&&nextTarget[a][b]!=6) {
+						
+						nextTarget[a][b]=8;
 						a+=left[j];
 						b+=right[j];
 					}
@@ -163,8 +171,9 @@ public class Surveillance {
 					int c = k[0]+left[(j+2)%4];
 					int d = k[1]+right[(j+2)%4];
 					
-					while(c>=0&&d>=0&&c<m&&d<n&&target[c][d]!=6) {
-						target[c][d]=8;
+					while(c>=0&&d>=0&&c<m&&d<n&&nextTarget[c][d]!=6) {
+						
+						nextTarget[c][d]=8;
 						c+=left[(j+2)%4];
 						d+=right[(j+2)%4];
 					}
@@ -172,68 +181,55 @@ public class Surveillance {
 					int e = k[0]+left[(j+3)%4];
 					int f = k[1]+right[(j+3)%4];
 					
-					while(e>=0&&f>=0&&e<m&&f<n&&target[e][f]!=6) {
-						target[e][f]=8;
+					while(e>=0&&f>=0&&e<m&&f<n&&nextTarget[e][f]!=6) {
+						
+						nextTarget[e][f]=8;
 						e+=left[(j+3)%4];
 						f+=right[(j+3)%4];
 					}
-					for (int fd = 0; fd < m; fd++) {
-						for (int j2 = 0; j2 < n; j2++) {
-							if (j2==n-1) {
-								System.out.println(target[fd][j2]);
-							}else {
-								System.out.print(target[fd][j2]);
-							}
-						}
-					}System.out.println("---------------------");
-					if (check(target, m, n)<result) {
-						result = check(target, m, n);
-					}
 					
-					surveil(target,origin,m,n,key,value,i);
 					
-					for (int l = 0; l < m; l++) {
-						for (int l2 = 0; l2 < n; l2++) {
-							target[l][l2]=origin[l][l2];
-						}
-					}
-					i++;
+					
+					surveil(nextTarget,origin,m,n,key,value,i+1);
+					
+//					for (int l = 0; l < m; l++) {
+//						for (int l2 = 0; l2 < n; l2++) {
+//							target[l][l2]=origin[l][l2];
+//						}
+//					}
+					
 				}
 			}else if(v==5){
+				int[][] nextTarget = new int[m][n];
+				for (int l = 0; l < m; l++) {
+					for (int l2 = 0; l2 < n; l2++) {
+						nextTarget[l][l2]=target[l][l2];
+					}
+				}
 				for (int j = 0; j < 4; j++) {
+					
 					int a = k[0]+left[j];
 					int b = k[1]+right[j];
 					
-					while(a>=0&&b>=0&&a<m&&b<n&&target[a][b]!=6) {
-						target[a][b]=8;
+					while(a>=0&&b>=0&&a<m&&b<n&&nextTarget[a][b]!=6) {
+						
+						nextTarget[a][b]=8;
 						a+=left[j];
 						b+=right[j];
 					}
-						
+					
 					
 				}
-				for (int fd = 0; fd < m; fd++) {
-					for (int j2 = 0; j2 < n; j2++) {
-						if (j2==n-1) {
-							System.out.println(target[fd][j2]);
-						}else {
-							System.out.print(target[fd][j2]);
-						}
-					}
-				}System.out.println("---------------------");
-				if (check(target, m, n)<result) {
-					result = check(target, m, n);
-				}
-				surveil(target,origin,m,n,key,value,i);
-				i++;
+				surveil(nextTarget,origin,m,n,key,value,i+1);
+				
+				
+				
 			}
 			
-			if (check(target, m, n)<result) {
-				result = check(target, m, n);
-			}
+			
 		
 	
-		}
+		
 		
 	}
 	public static void main(String[] args) {
@@ -245,8 +241,11 @@ public class Surveillance {
 		int[][] origin = new int[m][n];
 		result = m*n;
 		
-		Queue<int[]> key = new LinkedList<int[]>();
+		
+		
 		Queue<Integer> value = new LinkedList<Integer>();
+		
+		List<int[]> key = new ArrayList<int[]>();
 		
 		for (int i = 0; i < m; i++) {
 			for (int j = 0; j < n; j++) {
