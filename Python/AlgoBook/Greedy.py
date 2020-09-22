6abac636ad258417b07e190288ba0f195b682247
insunkang <33975738+insunkang@users.noreply.github.com>
2020-09-22 22:01:40 +0900

    update algobook

--- Python/AlgoBook/Greedy.py
+++ Python/AlgoBook/Greedy.py
@@ -31,15 +31,31 @@ import time
 # print(end_time-start_time)
 
 # 숫자 카드 게임
-start_time = time.time()
-a,b = map(int, input().split())
-tot = 0
+# start_time = time.time()
+# a,b = map(int, input().split())
+# tot = 0
+#
+# for _ in range(a):
+#     target = list(map(int,input().split()))
+#     mini = min(target)
+#     if mini>tot:
+#         tot=mini
+# end_time = time.time()
+# print(tot)
+# print(end_time-start_time)
 
-for _ in range(a):
-    target = list(map(int,input().split()))
-    mini = min(target)
-    if mini>tot:
-        tot=mini
-end_time = time.time()
-print(tot)
-print(end_time-start_time)
+# 상하좌우
+n = int(input())
+move = list(map(str,input().split()))
+present = [0,0]
+for i in range(len(move)):
+    if move[i]=="L" and n>present[1]-1>=0 :
+        present[1]=present[1]-1
+    elif move[i]=="R" and n>present[1]+1>=0:
+        present[1] = present[1] + 1
+    elif move[i]=="U" and n>present[0]-1>=0:
+        present[0] = present[0] - 1
+    elif move[i]=="D" and n>present[0]+1>=0:
+        present[0] = present[0] + 1
+
+print(present[0]+1,present[1]+1)
