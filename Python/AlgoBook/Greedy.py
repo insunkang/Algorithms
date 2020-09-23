d5d699a2d8a47e9a7f011dfd6c2648d703e9d271
insunkang <33975738+insunkang@users.noreply.github.com>
2020-09-23 14:48:17 +0900

    update algobook

--- Python/AlgoBook/Greedy.py
+++ Python/AlgoBook/Greedy.py
@@ -45,17 +45,156 @@ import time
 # print(end_time-start_time)
 
 # 상하좌우
-n = int(input())
-move = list(map(str,input().split()))
-present = [0,0]
-for i in range(len(move)):
-    if move[i]=="L" and n>present[1]-1>=0 :
-        present[1]=present[1]-1
-    elif move[i]=="R" and n>present[1]+1>=0:
-        present[1] = present[1] + 1
-    elif move[i]=="U" and n>present[0]-1>=0:
-        present[0] = present[0] - 1
-    elif move[i]=="D" and n>present[0]+1>=0:
-        present[0] = present[0] + 1
+# n = int(input())
+# move = list(map(str,input().split()))
+# present = [0,0]
+# for i in range(len(move)):
+#     if move[i]=="L" and n>present[1]-1>=0 :
+#         present[1]=present[1]-1
+#     elif move[i]=="R" and n>present[1]+1>=0:
+#         present[1] = present[1] + 1
+#     elif move[i]=="U" and n>present[0]-1>=0:
+#         present[0] = present[0] - 1
+#     elif move[i]=="D" and n>present[0]+1>=0:
+#         present[0] = present[0] + 1
+#
+# print(present[0]+1,present[1]+1)
 
-print(present[0]+1,present[1]+1)
+# 왕실의 나이트
+# put = input()
+# n = put[0]
+# m = put[1]
+#
+# a=int(m)
+#
+# b=ord(n)-ord("a")
+# result = 0
+#
+# if 8>a+2>=0:
+#     if 8>b+1>=0:
+#         result+=1
+#     elif 8>b-1>=0:
+#         result+=1
+# if 8>a-2>=0:
+#     if 8>b+1>=0:
+#         result+=1
+#     elif 8>b-1>=0:
+#         result+=1
+#
+# if 8>b-2>=0:
+#     if 8>a+1>=0:
+#         result+=1
+#     elif 8>a-1>=0:
+#         result+=1
+#
+# if 8>b+2>=0:
+#     if 8>a+1>=0:
+#         result+=1
+#     elif 8>a-1>=0:
+#         result+=1
+#
+# print(result)
+
+
+# m,n = map(int,input().split())
+# x,y,dir = map(int,input().split())
+# target = [[_]*m for _ in range(n)]
+# dx = [-1, 0, 1, 0]
+# dy = [0, -1, 0, 1]
+# check = [[False]*m for _ in range(n)]
+# for k in range(m):
+#     i = list(map(int, input().split()))
+#     for n in range(len(i)):
+#         target[k][n]=i[n]
+# dircheck = dir
+# while(True):
+#
+#     if m>x+dx[dir]<0 or n>y+dy[dir]<0 or target[x+dx[dir]][y+dy[dir]]==0 or check[x+dx[dir]][y+dy[dir]]==True:
+#         if dir==3:
+#             dir==0
+#         else:
+#             dir+=1
+#         if dir==dircheck:
+#             if dir==0 and target[x+dx[2]][y+dy[2]]==0:
+#                 break
+#             else if target[x+dx[dir-2]]:
+#
+#
+#         continue
+#
+#
+# print(a)
+
+# 음료수 얼려먹기
+# m,n = map(int, input().split())
+# check = [[0] * n for _ in range(m)]
+# target =[]
+#
+# for i in range(m):
+#     target.append(list(map(int,input())))
+#
+# result = 0;
+# def dfs(target, i, j, m, n):
+#     dx = [-1, 0, 1, 0]
+#     dy = [0, -1, 0, 1]
+#
+#     check[i][j] = 1
+#
+#     for k in range(4):
+#         if m>i+dx[k]>=0 and n>j+dy[k]>=0 and target[i+dx[k]][j+dy[k]]==0 and check[i+dx[k]][j+dy[k]]==0:
+#             dfs(target, i + dx[k], j + dy[k], m, n)
+#
+#
+#
+# for i in range(m):
+#     for j in range(n):
+#         if target[i][j]==0 and check[i][j]==0:
+#             check[i][j] = 1
+#             result+=1
+#             dfs(target,i,j,m,n)
+#
+#
+# print(result)
+from collections import deque
+m,n = map(int,input().split())
+
+
+dx = [-1, 0, 1, 0]
+dy = [0, -1, 0, 1]
+
+check = [[0]*n for _ in range(m)]
+target = []
+
+for _ in range(m):
+    target.append(list(map(int,input())))
+print(target)
+def bfs( i, j):
+    q = deque()
+    q.append((i, j))
+
+    result = 1
+    while q:
+        o = 0
+        a, b = q.popleft()
+        check[a][b] = 1
+        print(q)
+        print(a,b)
+        if a == m-1 and b == n-1:
+            result-=len(q)
+            print(result)
+            return
+        else:
+
+            result += 1
+            for k in range(4):
+                if m > a+dx[k] >= 0 and n > b+dy[k] >= 0 and target[a+dx[k]][b+dy[k]] == 1 and check[a+dx[k]][b+dy[k]] == 0:
+                    q.append((a+dx[k],b+dy[k]))
+                else:
+                    o+=1
+            if o==4:
+                 result-=1
+
+
+
+check[0][0] = 1
+bfs(0,0)
--- src/algoBook/Greedy1.java
+++ src/algoBook/Greedy1.java
@@ -5,26 +5,95 @@ public class Greedy1 {
 	public static void main(String[] args) {
 //		1이 될때 까지
 		
-		int n = 25;
-		int k = 5;
-		int i = 0;
+//		int n = 25;
+//		int k = 5;
+//		int i = 0;
+//		while(true) {
+//			if (n==1) {
+//				break;
+//			}
+//			if(n%k!=0) {
+//				n=n-k;
+//				i++;
+//			}else {
+//				n=n/k;
+//				i++;
+//			}
+//			
+//			
+//		}
+//		
+//		System.out.println(i);
+//		왕실의 나이트
+//		String input = "a1";
+//		int n =8;
+//		String x = "a";
+//		int a = input.charAt(1)-'0'-1;
+//		int b = (int)input.charAt(0)-(int)x.charAt(0);
+//		int result =0 ;
+//		System.out.println("a"+a);
+//		System.out.println("b"+b);
+//		if (a-2>=0&&a-2<n) {
+//			if (b-1>=0&&b-1<n) {
+//				result++;
+//			}else if (b+1>=0&&b-1<n) {
+//				result++;
+//			}
+//		}
+//		
+//		if (a+2>=0&&a+2<n) {
+//			if (b-1>=0&&b-1<n) {
+//				result++;
+//			}else if (b+1>=0&&b-1<n) {
+//				result++;
+//			}
+//		}
+//		
+//		if (b+2>=0&&b+2<n) {
+//			if (a-1>=0&&a-1<n) {
+//				result++;
+//			}else if (a+1>=0&&a-1<n) {
+//				result++;
+//			}
+//		}
+//		
+//		if (b-2>=0&&b-2<n) {
+//			if (a-1>=0&&a-1<n) {
+//				result++;
+//			}else if (a+1>=0&&a-1<n) {
+//				result++;
+//			}
+//		}
+//		
+//		System.out.println(result);
+//	게임 개발
+		int m = 4;
+		int n = 4;
+		int[][] target = {{1, 1, 1, 1}, {1, 0, 0, 1}, {1, 1, 0, 1}, {1, 1, 1, 1}};
+		boolean[][] check = new boolean[m][n];
+		int dir = 0;
+		int[] present = {0,0};
+		int[] dx = {-1, 0, 1, 0};
+		int[] dy = {0, -1, 0, 1};
+		
+		int dircheck = 0;
 		while(true) {
-			if (n==1) {
-				break;
+			if (present[0]+dx[dir]<0&&present[0]+dx[dir]>3&&present[1]+dy[dir]<0&&present[1]+dy[dir]>3||target[present[0]+dx[dir]][present[1]+dy[dir]]==0) {
+				if(dir==3) {
+					dir=0;
+				}else {
+					dir++;
+				}
+				dircheck++;
+				if (dircheck==4) {
+					break;
+				}
+				continue;
 			}
-			if(n%k!=0) {
-				n=n-k;
-				i++;
-			}else {
-				n=n/k;
-				i++;
-			}
-			
-			
 		}
 		
-		System.out.println(i);
-
+		
+		
 	}
 
 }
