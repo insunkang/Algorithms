d98b296c9ef01dfe726ba2f281950160e85e82fb
insunkang <33975738+insunkang@users.noreply.github.com>
2020-09-29 14:29:04 +0900

    update python code

--- Python/exam/11st.py
+++ Python/exam/11st.py
@@ -14,20 +14,49 @@ A.sort()
 # minOfA = min(A)
 # maxOfA = max(A)
 
-check = 1
-result = 0
-for i in range(len(A)):
-    if A[i] != check :
-        while(A[i] != check):
-            result += 1
+# check = 1
+# result = 0
+# for i in range(len(A)):
+#     if A[i] != check :
+#         while(A[i] != check):
+#             result += 1
+#
+#             if A[i]>check:
+#                 A[i]-=1
+#
+#             else:
+#                 A[i]+=1
+#     check+=1
+#     if result > 1000000000:
+#         result=-1
+#         break
+# print(result)
 
-            if A[i]>check:
-                A[i]-=1
+asd = ["zzzz","ferz","zdsr","fgtd"]
 
-            else:
-                A[i]+=1
-    check+=1
-    if result > 1000000000:
-        result=-1
-        break
-print(result)
+
+def solution(S):
+    dic = dict()
+    for i in range(len(S)):
+        for j in range(len(S[i])):
+            dic[j] = dic.get(j, []) + [(S[i][j], i)]
+    print(dic)
+    for i in range(len(dic)):
+        dic[i].sort()
+    answer = []
+    if len(dic) > 0:
+        for i in range(0, len(dic)):
+            for j in range(1, len(dic[i])):
+                if dic[i][j][0] == dic[i][j - 1][0]:
+                    answer = [dic[i][j - 1][1], dic[i][j][1], i]
+    # return answer
+    print(answer)
+solution(asd)
+
+# for (start, end) in tickets:
+#         print(start, end)
+#         routes[start] = routes.get(start, []) + [end]
+#         print(routes[start])
+#
+#  for (start, end) in tickets:
+#         routes[start] = routes.get(start, []) + [end]
