6a00cd9fee80b1f93f14fc59da7024b3f50cf54e
insunkang <33975738+insunkang@users.noreply.github.com>
2020-10-02 16:10:57 +0900

    update floor construction dp problem

--- Python/AlgoBook/DPProblems.py
+++ Python/AlgoBook/DPProblems.py
@@ -32,21 +32,48 @@
 
 
 #  개미 전사
+# n = int(input())
+#
+# # 모든 식량 정보 입력 받기
+# array = list(map(int, input().split()))
+# #  앞서 계산된 결과를 저장히기 위한 DP 테이블 초기화
+# d = [0]*100
+#
+# # 다이나믹 프로그래밍(Dynamic Programming) 진행 (바텀업)
+# d[0] = array[0]
+# d[1] = max(array[0],array[1])
+#
+# for i in range(2,n):
+#     d[i] = max(d[i-1], d[i-2] + array[i])
+#
+# # 계산된 결과 출력
+# print(d[n-1])
+
+# 바닥공사
+
+d = [0]*1000
+
 n = int(input())
 
-# 모든 식량 정보 입력 받기
-array = list(map(int, input().split()))
-#  앞서 계산된 결과를 저장히기 위한 DP 테이블 초기화
-d = [0]*100
+d[0] = 1
+d[1] = 3
+for i in range(2, n):
+    d[i] = (d[i-1] + 2 * d[i-2]) % 796796
 
-# 다이나믹 프로그래밍(Dynamic Programming) 진행 (바텀업)
-d[0] = array[0]
-d[1] = max(array[0],array[1])
-
-for i in range(2,n):
-    d[i] = max(d[i-1], d[i-2] + array[i])
-
-# 계산된 결과 출력
 print(d[n-1])
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
