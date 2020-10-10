bd517ba15b7721194f0954061702ae66a22c4870
insunkang <33975738+insunkang@users.noreply.github.com>
2020-10-10 17:03:19 +0900

    복습

--- Python/AlgoBook/Dijkstra.py
+++ Python/AlgoBook/Dijkstra.py
@@ -125,47 +125,93 @@
 #     else:
 #         print(distance[i])
 
-# 플로이드-워셜 알고리즘
-INF = int(1e9) # 무한을 의미하는 값으로 10억을 설정
+# # 플로이드-워셜 알고리즘
+# INF = int(1e9) # 무한을 의미하는 값으로 10억을 설정
 
-# 노드의 개수 및 간선의 개수를 입력받기
-n = int(input())
-m = int(input())
+# # 노드의 개수 및 간선의 개수를 입력받기
+# n = int(input())
+# m = int(input())
 
-# 2차원 리스트(그래프 표햔)을 만들고, 모든 값을 무한으로 초기화
-graph = [[INF] * (n + 1) for _ in range(n + 1)]
+# # 2차원 리스트(그래프 표햔)을 만들고, 모든 값을 무한으로 초기화
+# graph = [[INF] * (n + 1) for _ in range(n + 1)]
 
-# 자기 자신에서 자기 자신으로 가는 비용은 0으로 초기회
-for a in range(1, n + 1):
-    for b in range(1, n + 1):
-        if a == b:
-            graph[a][b] = 0
+# # 자기 자신에서 자기 자신으로 가는 비용은 0으로 초기회
+# for a in range(1, n + 1):
+#     for b in range(1, n + 1):
+#         if a == b:
+#             graph[a][b] = 0
 
-# 각 간선에 대한 정보를 입력받아, 그 값으로 초기화
-for _ in range(m):
-    # A에서 B로 가는 비용은 C라고 설정
-    a, b, c = map(int, input().split())
-    graph[a][b] = c
+# # 각 간선에 대한 정보를 입력받아, 그 값으로 초기화
+# for _ in range(m):
+#     # A에서 B로 가는 비용은 C라고 설정
+#     a, b, c = map(int, input().split())
+#     graph[a][b] = c
 
-# 점화식에 따라 플로이드 워셜 알고리즘을 수행
-for k in range(1, n + 1):
-    for a in range(1, n + 1):
-        for b in range(1, n + 1):
-            graph[a][b] = min(graph[a][b], graph[a][k] + graph[k][b])
-
-
-# 수행된 결과를 출력
-for a in range(1, n + 1):
-    for b in range(1, n + 1):
-        # 도달할 수 없는 경우, 무한(INFINITY)이라고 출력
-        if graph[a][b] == INF:
-            print("INFINITY", end=" ")
-        # 도달할 수 있는 경우 거리를 출력
-        else:
-            print(graph[a][b], end=" ")
-    print()
+# # 점화식에 따라 플로이드 워셜 알고리즘을 수행
+# for k in range(1, n + 1):
+#     for a in range(1, n + 1):
+#         for b in range(1, n + 1):
+#             graph[a][b] = min(graph[a][b], graph[a][k] + graph[k][b])
 
 
+# # 수행된 결과를 출력
+# for a in range(1, n + 1):
+#     for b in range(1, n + 1):
+#         # 도달할 수 없는 경우, 무한(INFINITY)이라고 출력
+#         if graph[a][b] == INF:
+#             print("INFINITY", end=" ")
+#         # 도달할 수 있는 경우 거리를 출력
+#         else:
+#             print(graph[a][b], end=" ")
+#     print()
+
+# import heapq
+# import sys
+# input = sys.stdin.readline
+
+# INF = int(1e9)
+
+# n, m = map(int, input().split())
+
+# start = int(input())
+
+# graph = [[] for i in range(n + 1)]
+
+# distance = [INF] * (n + 1)
+
+# for _ in range(m):
+#     a, b, c = map(int, input().split())
+#     graph[a].append((b,c))
+
+# print(graph)
+
+
+
+# def dijkstra(start):
+#     q = []
+#     heapq.heappush(q, (0, start))
+#     distance[start] = 0    
+
+#     while q:
+#         dist, now = heapq.heappop(q)
+
+#         if distance[now] < dist:
+#             continue
+
+#         for i in graph[now]:
+#             cost = dist + i[1]
+
+#             if cost<distance[i[0]]:
+#                 distance[i[0]] = cost
+#                 heapq.heappush(q, (cost, i[0]))
+
+# dijkstra(start)        
+
+# for i in range(1, n + 1):
+#     if distance[i] == INF:
+#         print("INFINITY")
+#     else:
+#         print(distance[i])
 
 
 
