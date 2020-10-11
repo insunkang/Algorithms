655292c22d6617a9b3f4af9ba326f37529a573a0
insunkang <33975738+insunkang@users.noreply.github.com>
2020-10-11 17:44:51 +0900

    복습2

--- Python/AlgoBook/Dijkstra.py
+++ Python/AlgoBook/Dijkstra.py
@@ -213,6 +213,45 @@
 #     else:
 #         print(distance[i])
 
+import sys
+input = sys.stdin.readline
+INF = int(1e9)
+n = int(input())
+m = int(input())
+graph = [[INF] * (n + 1) for _ in range(n + 1)]
+
+for i in range(m):
+    a, b, c = map(int, input().split())
+    graph[a][b] = c
+
+for i in range(1, n + 1):
+    for j in range(1, n + 1):
+        if i==j:
+            graph[i][j]=0
+
+for k in range(1, n + 1):
+    for i in range(1, n + 1):
+        for j in range(1, n + 1):
+            graph[i][j] = min(graph[i][j], graph[i][k]+graph[k][j])
+
+
+# print(graph)
+
+for i in range(1, n + 1):
+    for j in range(1, n + 1):
+        if graph[i][j] == INF:
+            if j == n:
+                print("INFINITY")                
+            else:
+                print("INFINITY", end=" ")
+            
+        else:
+            if j == n:
+                print(graph[i][j])
+            else:
+                print(graph[i][j], end=" ")
+            
+
 
 
 
