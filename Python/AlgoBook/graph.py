3bbb462988c3d3f5ca6fe7d21c90a3a2b5a98f71
insunkang <33975738+insunkang@users.noreply.github.com>
2020-10-22 15:27:05 +0900

    solve the plan of split city

--- Python/AlgoBook/graph.py
+++ Python/AlgoBook/graph.py
@@ -71,36 +71,75 @@
 
 # print(result)
 
+# import sys
+
+# input = sys.stdin.readline
+
+# node, cal = map(int, input().split())
+
+# team = [i for i in range(node+1)]
+
+# def findparents(team, a):
+#     if team[a] != a:
+#         team[a] = findparents(team,team[a])
+#     return team[a]
+
+# def unionparent(team,a,b):
+#     a = findparents(team,a)
+#     b = findparents(team,b)
+
+#     if a>b:
+#         team[a]=b
+#     else:
+#         team[b]=a
+# result = []
+# for _ in range(cal):
+#     check, a, b = map(int,input().split())
+#     if check == 0 :
+#         unionparent(team,a,b)
+#     else:
+#         if findparents(team,a) == findparents(team,b):
+#             result.append("YES")
+#         else:
+#             result.append("NO")
+
+# print(result)
+
 import sys
 
 input = sys.stdin.readline
 
-node, cal = map(int, input().split())
+node, vertex = map(int, input().split())
 
-team = [i for i in range(node+1)]
+parents = [i for i in range(node+1)]
+vertexes = []
+def findparents(parents,a):
+    if parents[a]!=a:
+        parents[a] = findparents(parents,parents[a])
+    
+    return parents[a]
 
-def findparents(team, a):
-    if team[a] != a:
-        team[a] = findparents(team,team[a])
-    return team[a]
-
-def unionparent(team,a,b):
-    a = findparents(team,a)
-    b = findparents(team,b)
+def unionparent(parents,a,b):
+    a = findparents(parents,a)
+    b = findparents(parents,b)
 
     if a>b:
-        team[a]=b
+        parents[a]=b
     else:
-        team[b]=a
-result = []
-for _ in range(cal):
-    check, a, b = map(int,input().split())
-    if check == 0 :
-        unionparent(team,a,b)
-    else:
-        if findparents(team,a) == findparents(team,b):
-            result.append("YES")
-        else:
-            result.append("NO")
+        parents[b]=a
 
-print(result)
+for _ in range(vertex):
+    a,b,cost = map(int, input().split())
+    vertexes.append((cost,a,b))
+
+vertexes.sort()
+
+result = 0
+last = 0
+for i in vertexes:
+    cost,a,b = i
+    if findparents(parents,a) != findparents(parents,b):
+        unionparent(parents,a,b)
+        result+=cost
+        last = cost
+print(result - last)
