7977c7edde5e362cbb614bae9ad60fac9d0d77c4
insunkang <33975738+insunkang@users.noreply.github.com>
2020-10-31 15:25:27 +0900

    re-solve muji mukbang live

--- Python/AlgoBook/RealP.py
+++ Python/AlgoBook/RealP.py
@@ -50,17 +50,17 @@
 
 # Unavailable cost
 
-n = int(input())
-money = list(map(int, input().split()))
-money.sort()
-target = 1
+# n = int(input())
+# money = list(map(int, input().split()))
+# money.sort()
+# target = 1
 
-for x in money:
-    if target<x:
-        break
-    target += x
+# for x in money:
+#     if target<x:
+#         break
+#     target += x
 
-print(target)
+# print(target)
 # select bowling ball
 
 # n,m = map(int, input().split())
@@ -75,3 +75,26 @@ print(target)
 #             result+=1
 
 # print(result)
+
+import heapq
+
+def solution(food_times, k):
+    if sum(food_times) <= k:
+        return -1
+    
+    q = []
+    for i in range(len(food_times)):
+        heapq.heappush(q,(food_times[i], i + 1))
+
+    sum_value = 0
+    previous = 0
+    length = len(food_times)
+
+    while sum_value + ((q[0][0] - previous) * length) <= k:
+        now = heapq.heappop(q)[0]
+        sum_value += (now - previous) * length
+        length -= 1
+        previous = now
+
+    result = sorted(q, key = lambda x: x[1])
+    return result[(k - sum_value) % length][1]
