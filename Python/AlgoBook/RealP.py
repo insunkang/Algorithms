251dc3fb0936dd21e0f284e25f2f3d61387efd90
insunkang <33975738+insunkang@users.noreply.github.com>
2020-11-03 13:51:21 +0900

    solve lock and key

--- Python/AlgoBook/RealP.py
+++ Python/AlgoBook/RealP.py
@@ -126,32 +126,72 @@
 #     print("READY")
 
 # 문자열 재정렬
-stringSet  = str(input())
+# stringSet  = str(input())
 
-result = []
-for i in stringSet:
-    result.append(i)
+# result = []
+# for i in stringSet:
+#     result.append(i)
 
-result.sort()
+# result.sort()
 
-a = 0
+# a = 0
 
-b = ""
+# b = ""
 
-print(result)
+# print(result)
 
-for i in result:
-    if ord(i)<=64:
-        a+=int(i)
-    else:
-        b+=i
+# for i in result:
+#     if ord(i)<=64:
+#         a+=int(i)
+#     else:
+#         b+=i
 
-print(a,b)
+# print(a,b)
 
-answer = ""
-answer += b
-answer += str(a)
+# answer = ""
+# answer += b
+# answer += str(a)
 
-print(answer)
+# print(answer)
 
+# 문자열 압축
 
+# 자물쇠와 열쇠
+def lotate(a):
+    result = [[0] * len(a) for _ in range(len(a))]
+    for i in range(len(a)):
+        for j in range(len(a)):
+            result[j][len(a)-i-1] = a[i][j]
+    return result
+
+def check(new_lock):
+    newlen = len(new_lock)//3
+    for i in range(newlen,newlen*2):
+        for j in range(newlen,newlen*2):
+            if new_lock[i][j]!=1:
+                return False
+    return True
+            
+def solution(key, lock):
+    m = len(key)
+    n = len(lock)
+    new_lock = [[0] * (n*3) for _ in range(n*3)]
+    for i in range(n):
+        for j in range(n):
+            new_lock[i+n][j+n] = lock[i][j]
+            
+    for _ in range(4):
+        key = lotate(key)
+        for x in range(n*2):
+            for y in range(n*2):
+                
+                for i in range(m):
+                    for j in range(m):
+                        new_lock[x + i][y + j] += key[i][j]
+                if check(new_lock) == True:
+                    return True
+                for i in range(m):
+                    for j in range(m):
+                        new_lock[x + i][y+ j] -= key[i][j]
+                
+    return False
