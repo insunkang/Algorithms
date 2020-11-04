768ce3b714c03e1d00f5de2ea964fccb9bad747e
insunkang <33975738+insunkang@users.noreply.github.com>
2020-11-04 17:10:20 +0900

    solve snake

--- Python/AlgoBook/RealP.py
+++ Python/AlgoBook/RealP.py
@@ -157,41 +157,105 @@
 # 문자열 압축
 
 # 자물쇠와 열쇠
-def lotate(a):
-    result = [[0] * len(a) for _ in range(len(a))]
-    for i in range(len(a)):
-        for j in range(len(a)):
-            result[j][len(a)-i-1] = a[i][j]
-    return result
+# def lotate(a):
+#     result = [[0] * len(a) for _ in range(len(a))]
+#     for i in range(len(a)):
+#         for j in range(len(a)):
+#             result[j][len(a)-i-1] = a[i][j]
+#     return result
 
-def check(new_lock):
-    newlen = len(new_lock)//3
-    for i in range(newlen,newlen*2):
-        for j in range(newlen,newlen*2):
-            if new_lock[i][j]!=1:
-                return False
-    return True
+# def check(new_lock):
+#     newlen = len(new_lock)//3
+#     for i in range(newlen,newlen*2):
+#         for j in range(newlen,newlen*2):
+#             if new_lock[i][j]!=1:
+#                 return False
+#     return True
             
-def solution(key, lock):
-    m = len(key)
-    n = len(lock)
-    new_lock = [[0] * (n*3) for _ in range(n*3)]
-    for i in range(n):
-        for j in range(n):
-            new_lock[i+n][j+n] = lock[i][j]
+# def solution(key, lock):
+#     m = len(key)
+#     n = len(lock)
+#     new_lock = [[0] * (n*3) for _ in range(n*3)]
+#     for i in range(n):
+#         for j in range(n):
+#             new_lock[i+n][j+n] = lock[i][j]
             
-    for _ in range(4):
-        key = lotate(key)
-        for x in range(n*2):
-            for y in range(n*2):
+#     for _ in range(4):
+#         key = lotate(key)
+#         for x in range(n*2):
+#             for y in range(n*2):
                 
-                for i in range(m):
-                    for j in range(m):
-                        new_lock[x + i][y + j] += key[i][j]
-                if check(new_lock) == True:
-                    return True
-                for i in range(m):
-                    for j in range(m):
-                        new_lock[x + i][y+ j] -= key[i][j]
+#                 for i in range(m):
+#                     for j in range(m):
+#                         new_lock[x + i][y + j] += key[i][j]
+#                 if check(new_lock) == True:
+#                     return True
+#                 for i in range(m):
+#                     for j in range(m):
+#                         new_lock[x + i][y+ j] -= key[i][j]
                 
-    return False
+#     return False
+
+import sys
+input = sys.stdin.readline
+
+q = [(0,0)]
+
+
+#   오 아 왼 위
+x = [0,1,0,-1]
+y = [1,0,-1,0]
+cHead = 0
+cLoc= [0,0]
+cLotate = 0
+time = 0
+length = int(input())
+board = [[0]*length for _ in range(length)]
+apple = int(input())
+
+for _ in range(apple):
+    i,j = map(int,input().split())
+    board[i-1][j-1] = 1
+
+lotate = int(input())
+lotateA = []
+board[0][0] = 2
+for _ in range(lotate):
+    lotateA.append(list(map(str,input().split())))
+
+while True:
+    time+=1
+    a=cLoc[0]+x[cHead]
+    b=cLoc[1]+y[cHead]
+    if length>a>=0 and length>b>=0 and board[a][b]!=2:
+        cLoc=[a,b]
+        q.append((a,b))
+       
+        if board[a][b]==0:
+            dx,dy = q.pop(0)
+            board[dx][dy] = 0
+            board[a][b]=2
+        if board[a][b]==1:
+            board[a][b]=2 
+        
+        if cLotate<len(lotateA) and int(lotateA[cLotate][0])==time:
+            if lotateA[cLotate][1]=="D":
+                if cHead==3:
+                    cHead=0
+                else:
+                    cHead+=1
+            else:
+                if cHead==0:
+                    cHead=3
+                else:
+                    cHead-=1
+            cLotate+=1
+        
+    else:
+        break
+    
+
+print(time)
+print(q)
+print(board)
+print(lotateA)
