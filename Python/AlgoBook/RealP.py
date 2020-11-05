d07011178cac503d1bee6b6c79cfbecc76b1f6b9
insunkang <33975738+insunkang@users.noreply.github.com>
2020-11-05 15:20:41 +0900

    practice columns and beams

--- Python/AlgoBook/RealP.py
+++ Python/AlgoBook/RealP.py
@@ -195,67 +195,102 @@
 #                         new_lock[x + i][y+ j] -= key[i][j]
                 
 #     return False
+# 뱀
+# import sys
+# input = sys.stdin.readline
 
-import sys
-input = sys.stdin.readline
-
-q = [(0,0)]
+# q = [(0,0)]
 
 
-#   오 아 왼 위
-x = [0,1,0,-1]
-y = [1,0,-1,0]
-cHead = 0
-cLoc= [0,0]
-cLotate = 0
-time = 0
-length = int(input())
-board = [[0]*length for _ in range(length)]
-apple = int(input())
+# #   오 아 왼 위
+# x = [0,1,0,-1]
+# y = [1,0,-1,0]
+# cHead = 0
+# cLoc= [0,0]
+# cLotate = 0
+# time = 0
+# length = int(input())
+# board = [[0]*length for _ in range(length)]
+# apple = int(input())
 
-for _ in range(apple):
-    i,j = map(int,input().split())
-    board[i-1][j-1] = 1
+# for _ in range(apple):
+#     i,j = map(int,input().split())
+#     board[i-1][j-1] = 1
 
-lotate = int(input())
-lotateA = []
-board[0][0] = 2
-for _ in range(lotate):
-    lotateA.append(list(map(str,input().split())))
+# lotate = int(input())
+# lotateA = []
+# board[0][0] = 2
+# for _ in range(lotate):
+#     lotateA.append(list(map(str,input().split())))
 
-while True:
-    time+=1
-    a=cLoc[0]+x[cHead]
-    b=cLoc[1]+y[cHead]
-    if length>a>=0 and length>b>=0 and board[a][b]!=2:
-        cLoc=[a,b]
-        q.append((a,b))
+# while True:
+#     time+=1
+#     a=cLoc[0]+x[cHead]
+#     b=cLoc[1]+y[cHead]
+#     if length>a>=0 and length>b>=0 and board[a][b]!=2:
+#         cLoc=[a,b]
+#         q.append((a,b))
        
-        if board[a][b]==0:
-            dx,dy = q.pop(0)
-            board[dx][dy] = 0
-            board[a][b]=2
-        if board[a][b]==1:
-            board[a][b]=2 
+#         if board[a][b]==0:
+#             dx,dy = q.pop(0)
+#             board[dx][dy] = 0
+#             board[a][b]=2
+#         if board[a][b]==1:
+#             board[a][b]=2 
         
-        if cLotate<len(lotateA) and int(lotateA[cLotate][0])==time:
-            if lotateA[cLotate][1]=="D":
-                if cHead==3:
-                    cHead=0
-                else:
-                    cHead+=1
-            else:
-                if cHead==0:
-                    cHead=3
-                else:
-                    cHead-=1
-            cLotate+=1
+#         if cLotate<len(lotateA) and int(lotateA[cLotate][0])==time:
+#             if lotateA[cLotate][1]=="D":
+#                 if cHead==3:
+#                     cHead=0
+#                 else:
+#                     cHead+=1
+#             else:
+#                 if cHead==0:
+#                     cHead=3
+#                 else:
+#                     cHead-=1
+#             cLotate+=1
         
-    else:
-        break
+#     else:
+#         break
     
 
-print(time)
-print(q)
-print(board)
-print(lotateA)
+# print(time)
+# print(q)
+# print(board)
+# print(lotateA)
+
+# 기둥과 보 
+def possible(answer):
+    for x,y,stuff in answer:
+        if stuff == 0:
+            if y == 0 or [x-1,y,1] in answer or [x,y,1] in answer or [x,y-1,0] in answer:
+                continue
+            return False
+        elif stuff == 1:
+            if [x,y-1,0] in answer or [x+1,y-1,0] in answer or ([x-1,y,1] in answer and [x+1,y,1] in answer):
+                continue
+            return False
+        return True
+
+def solution(n, build_frame):
+    answer = []
+#     [x,y,a,b] x,y 좌표, a 0 기둥 1 보, b 0삭제 1설치
+    for frame in build_frame:
+        # x=i[0]
+        # y=i[1]
+        # a=i[2]
+        # b=i[3]
+        x,y,stuff,operate = frame
+        if operate == 0:
+            answer.remove([x, y, stuff])
+            if not possible(answer):
+                answer.append([x, y, stuff])
+
+        if operate == 1:
+            answer.append([x,y,stuff])
+            if not possible(answer):
+                answer.remove([x ,y ,stuff])                
+    print(sorted(answer))
+
+solution(5,[[1,0,0,1],[1,1,1,1],[2,1,0,1],[2,2,1,1],[5,0,0,1],[5,1,0,1],[4,2,1,1],[3,2,1,1]])
