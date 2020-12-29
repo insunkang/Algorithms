614c9b36cfc42f876c65fb8bc44db44d27c3ce75
insunkang <33975738+insunkang@users.noreply.github.com>
2020-12-29 13:50:07 +0900

    update maketherunProgrammers

--- Python/AlgoBook/RealP_DBFS.py
+++ Python/AlgoBook/RealP_DBFS.py
@@ -1,38 +1,45 @@
-import sys
-from collections import deque
-input = sys.stdin.readline
+# import sys
+# from collections import deque
+# input = sys.stdin.readline
 
-n,m,k,x = map(int, input().split())
+# n,m,k,x = map(int, input().split())
 
-cities = [[] for _ in range(n+1)]
+# cities = [[] for _ in range(n+1)]
 
-nearest = [-1] * (n+1)
+# nearest = [-1] * (n+1)
 
-q= deque()
+# q= deque()
 
-nearest[x] = 0
+# nearest[x] = 0
 
-for _ in range(m):
-    i,j = map(int,input().split())
-    cities[i].append(j)
-
-
-q.append(x)
+# for _ in range(m):
+#     i,j = map(int,input().split())
+#     cities[i].append(j)
 
 
-while q:
-    now = q.popleft()
-    for i in cities[now]:
-        if nearest[i]==-1:
-            nearest[i]=nearest[now]+1
-            q.append(i)        
+# q.append(x)
 
 
-if k in nearest:
-    for i in range(1,len(nearest)):
-        if nearest[i] == k :
-            print(i)
-else:
-    print(-1)
+# while q:
+#     now = q.popleft()
+#     for i in cities[now]:
+#         if nearest[i]==-1:
+#             nearest[i]=nearest[now]+1
+#             q.append(i)        
 
 
+# if k in nearest:
+#     for i in range(1,len(nearest)):
+#         if nearest[i] == k :
+#             print(i)
+# else:
+#     print(-1)
+ads = [20,30,35,17,1,36,18,17,2,8,9,27,16]
+result = 0
+for i in range(len(ads)-2):
+    for j in(i+1,len(ads)-1):
+        for k in (j+1,len(ads)):
+            if ads[i]<ads[j] and ads[i]>ads[k]:
+                result+=1
+
+print(result)
